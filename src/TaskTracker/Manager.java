cbb614fa35c8a5e4949058e925cdf06e18ac705a
AndreyUst <avustjuzhanin@yandex.ru>
2022-12-08 17:29:51 +0300

    Refactor: создать все методы для Task

--- src/TaskTracker/Manager.java
+++ src/TaskTracker/Manager.java
@@ -1,52 +1,86 @@
 package TaskTracker;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Manager {
     String[] status = {"NEW", "IN_PROGRESS", "DONE"};   // список статусов работы над задачей
-    Integer lastID; // здесь хранитися последний, сгенерированный id всех задач
-
+    Integer lastID; // здесь хранитися последний сгенерированный id всех задач
     private HashMap<Integer, Epic> epicTasks = new HashMap<>(); // для хранения всех TaskTracker.Epic задач
     private HashMap<Integer, Subtask> subtaskTasks = new HashMap<>(); // для хранения всех TaskTracker.Subtask задач
     private HashMap<Integer, Task> taskTasks = new HashMap<>(); // для хранения всех TaskTracker.Task задач
 
+    // МЕТОДЫ В РАЗРАБОТКЕ
+    // Получение списка всех подзадач определённого эпика.
+    //
 
 
-    public void updateTask(int id, Task newTask){     // обновляем задачу
-        taskTasks.put(id, newTask);
+    public void makeNewTask(String nameOfTask, String descriptionOfTask, String status) {   // новая задача
+        int uniqueId = makeID();
+        Task task = new Task(uniqueId, nameOfTask, descriptionOfTask, status);
+        taskTasks.put(uniqueId, task);           // сохранили объект, содержащий полное описание задачи
     }
-
-    public void makeTask(String nameOfTask, String descriptionOfTask, String status) {   // новая задача
-        int unidueId = makeID();
-        Task task = new Task(unidueId, nameOfTask, descriptionOfTask, status);
-        taskTasks.put(unidueId, task);           // сохранили объект, содержащий полное описание задачи
+    public void makeNewEpic(String nameOfTask, String descriptionOfTask, String status) {   // новая Эпик задача
+        int uniqueEpicId = makeID();
+        //makeNewSubtask(String nameOfTask, String descriptionOfTask, String status);
+        Epic epic = new Epic(uniqueEpicId, nameOfTask, descriptionOfTask, status);
+        epicTasks.put(uniqueEpicId, epic);           // сохранили объект, содержащий полное описание Epic задачи
     }
-
+    public void makeNewSubtask(String nameOfTask, String descriptionOfTask, String status) {   // новая Эпик задача
+        int uniqueSubtaskId = makeID();
+        Subtask subtask = new Subtask(uniqueSubtaskId, nameOfTask, descriptionOfTask, status);
+        subtaskTasks.put(uniqueSubtaskId, subtask);           // сохранили объект, содержащий полное описание subtask задачи
+    }
+    // далее МЕТОДЫ ДЛЯ ВСЕХ ЗАДАЧ
+    public ArrayList<Object> getListAllTasks() { //Получение списка всех задач
+        ArrayList<Object> taskEpicSubtaskList = new ArrayList<Object>();
+        for (Integer id : taskTasks.keySet()) {
+            taskEpicSubtaskList.add(taskTasks.get(id));
+        }
+        for (Integer id : epicTasks.keySet()) {
+            taskEpicSubtaskList.add(epicTasks.get(id));
+        }
+        for (Integer id : subtaskTasks.keySet()) {
+            taskEpicSubtaskList.add(subtaskTasks.get(id));
+        }
+        return taskEpicSubtaskList;
+    }
+    public void dellThemAll() {    //Удаление всех задач.
+        if (!taskTasks.isEmpty()) {
+            taskTasks.clear();
+        } if (!epicTasks.isEmpty()) {
+            epicTasks.clear();
+        } if (!subtaskTasks.isEmpty()) {
+            subtaskTasks.clear();
+        }
+    }
+    public void dellTaskById(int idForDell) {  //Удаление по идентификатору.
+        if (taskTasks.containsKey(idForDell)) {
+            taskTasks.remove(idForDell);
+        } else if (epicTasks.containsKey(idForDell)) {
+            epicTasks.remove(idForDell);
+        } else if (subtaskTasks.containsKey(idForDell)) {
+            subtaskTasks.remove(idForDell);
+        }
+    }
+    public Object getTaskById(int idForSearch) {    //Получение задачи по идентификатору.
+        if (taskTasks.containsKey(idForSearch)) {
+            return taskTasks.get(idForSearch);
+        } else if (epicTasks.containsKey(idForSearch)) {
+            return epicTasks.get(idForSearch);
+        } else if (subtaskTasks.containsKey(idForSearch)) {
+           return subtaskTasks.get(idForSearch);
+        }
+        return null;
+    }
     Integer makeID() {  // метод который генерирует id для всех классов, при обращении к нему
         int id = lastID + 1;
         lastID = id;
         return id;
     }
-
-
 }
-   /*
-    Для генерации идентификаторов можно использовать числовое поле класса менеджер, увеличивая его на 1,
-    когда нужно получить новое значение.
 
-    Менеджер
-    Кроме классов для описания задач, вам нужно реализовать класс для объекта-менеджера.
-    Он будет запускаться на старте программы и управлять всеми задачами.
-    В нём должны быть реализованы следующие функции:
-        Возможность хранить задачи всех типов.   Для этого вам нужно выбрать подходящую коллекцию.
-        Методы для каждого из типа задач(Задача/Эпик/Подзадача):
-        Получение списка всех задач.
-        Удаление всех задач.
-        Получение по идентификатору.
-        Создание. Сам объект должен передаваться в качестве параметра.
-        Обновление. Новая версия объекта с верным идентификатором передаётся в виде параметра.
-        Удаление по идентификатору.
-
+/*
         Дополнительные методы:
         Получение списка всех подзадач определённого эпика.
         Управление статусами осуществляется по следующему правилу:
