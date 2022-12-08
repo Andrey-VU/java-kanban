9a226b5d7ef3fdefe588d230deb32ee36db487d3
AndreyUst <avustjuzhanin@yandex.ru>
2022-12-08 07:48:49 +0300

    Project: Начать работу. Создать классы. Поля в Task. Первые методы в manager

---
+++ src/TaskTracker/Epic.java
@@ -0,0 +1,7 @@
+package TaskTracker;
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Integer> mySubtasksID;
+
+}
---
+++ src/TaskTracker/Subtask.java
@@ -0,0 +1,6 @@
+package TaskTracker;
+
+public class Subtask extends Task {
+    private Integer myEpicID;    // переменная для id верхнеуровневой задачи
+
+}
