fca45db5d4329ffcfcc32f46cdcdd982ff86303b
AndreyUst <avustjuzhanin@yandex.ru>
2022-12-08 17:31:37 +0300

    создать пустышки

--- src/TaskTracker/Epic.java
+++ src/TaskTracker/Epic.java
@@ -2,6 +2,17 @@ package TaskTracker;
 import java.util.ArrayList;
 
 public class Epic extends Task {
-    private ArrayList<Integer> mySubtasksID;
+    private ArrayList<Integer> mySubtasksID = new ArrayList<>();
 
+    public Epic(Integer id, String nameOfTask, String descriptionOfTask, String status) {
+        super(id, nameOfTask, descriptionOfTask, status);
+        //mySubtasksID.add(subtaskId);
+
+    }
 }
+
+/*
+Каждый эпик знает, какие подзадачи в него входят.
+Завершение всех подзадач эпика считается завершением эпика.
+
+ */
--- src/TaskTracker/Subtask.java
+++ src/TaskTracker/Subtask.java
@@ -1,6 +1,15 @@
 package TaskTracker;
 
 public class Subtask extends Task {
-    private Integer myEpicID;    // переменная для id верхнеуровневой задачи
+    private int myEpicID;    // переменная для id верхнеуровневой задачи
 
+    public Subtask(Integer id, String nameOfTask, String descriptionOfTask, String status) {
+        super(id, nameOfTask, descriptionOfTask, status);
+        //myEpicID = epicId;
+    }
 }
+
+    /*
+Для каждой подзадачи известно, в рамках какого эпика она выполняется.
+     */
+
