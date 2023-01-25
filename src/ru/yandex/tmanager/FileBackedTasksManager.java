65fbaa467edfa8376038fbdeed62c83443ff422b
Andrey-VU <avustjuzhanin@yandex.ru>
2023-01-25 21:58:45 +0300

    debug: собрать программу, начать отладку

--- src/ru/yandex/tmanager/FileBackedTasksManager.java
+++ src/ru/yandex/tmanager/FileBackedTasksManager.java
@@ -9,21 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileBackedTasksManager extends InMemoryTaskManager {
+    private static FileBackedTasksManager loadedFromFile;
     InMemoryTaskManager taskManager = new InMemoryTaskManager();
     InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
 
-    static void main(String[] args) {
-            /* реализуйте небольшой сценарий: Заведите несколько разных задач, эпиков и подзадач.
+    public static void main(String[] args) throws IOException {
+        File file = new File("storage.csv");
+        loadedFromFile = loadFromFile(file);
+    }
+
+/* реализуйте небольшой сценарий: Заведите несколько разных задач, эпиков и подзадач.
 Запросите некоторые из них, чтобы заполнилась история просмотра.
 Создайте новый FileBackedTasksManager менеджер из этого же файла.
 Проверьте, что история просмотра восстановилась верно и все задачи, эпики, подзадачи,
 которые были в старом, есть в новом менеджере.
      */
-     }
 
-    static FileBackedTasksManager loadFromFile(File file) throws IOException {   // восстанавливать
+
+     static FileBackedTasksManager loadFromFile(File file) throws IOException {   // восстанавливать
         // данные менеджера из файла при запуске
-        FileBackedTasksManager loadedFromFile = new FileBackedTasksManager();
         List<String> tmp = new ArrayList<>();
         FileReader reader = new FileReader(file);
         try (BufferedReader br = new BufferedReader(reader)) {
@@ -34,17 +38,45 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         } catch (IOException e) {
             System.out.println("Произошла ошибка во время чтения файла.");
         }
-        for (String s : tmp) {
-            if (s.split(",")[1].equals("Task")) {
-                loadedFromFile.makeNewTask(fromString(s));
-            } else if (s.split(",")[1].equals("Epic")) {
-                loadedFromFile.makeNewEpic((Epic) fromString(s));
-            } else {
-                loadedFromFile.makeNewSubtask((Subtask) fromString(s));
+        for (int i = 1; i < tmp.size(); i++) {
+            if ((loadedFromFile != null)) {
+                if (tmp.get(i).split(",")[1].equals("TASK")) {
+                    loadedFromFile.makeNewTask(fromString(tmp.get(i)));
+                } else if (tmp.get(i).split(",")[1].equals("EPIC")) {
+                    loadedFromFile.makeNewEpic((Epic) fromString(tmp.get(i)));
+                } else if (tmp.get(i).split(",")[1].equals("SUBTASK")) {
+                    loadedFromFile.makeNewSubtask((Subtask) fromString(tmp.get(i)));
+                } else if (!tmp.get(i).isBlank()) {
+                    for (Integer idForHistory : historyFromString(tmp.get(i))) {
+                        loadedFromFile.historyManager.linkLast(loadedFromFile.getTaskById(idForHistory));
+                    }
+                }
             }
         }
         return loadedFromFile;
     }
+
+    //  =========  для сохранения и восстановления менеджера истории из CSV  =======
+    static String historyToString(HistoryManager manager) {
+        StringBuilder builder = new StringBuilder();
+        for (Task task : manager.getHistory()) {
+            builder.append(task.getId() + ",");
+        }
+        builder.deleteCharAt(builder.lastIndexOf(","));
+        String stringHistory = builder.toString();
+        return stringHistory;
+    }
+
+    static List<Integer> historyFromString(String value) {
+        List<Integer> listOfId = new ArrayList<>();
+        if (value.length() > 2) {
+            for (String s : value.split(",")) {
+                listOfId.add(Integer.parseInt(s));
+            }
+        } else {listOfId.add(Integer.parseInt(value));}
+        return listOfId;
+    }
+
 
     static Task fromString(String s) {         // создание задачи из строки
         Task taskFromFile = null;
@@ -53,8 +85,8 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 int id = Integer.parseInt(s.split(",")[0]);
                 String type = s.split(",")[1];
                 String name = s.split(",")[2];
-                Status status = (s.split(",")[3].equals("New") ? Status.NEW
-                        : (s.split(",")[3].equals("Done") ? Status.DONE : Status.IN_PROGRESS));
+                Status status = (s.split(",")[3].equals("NEW") ? Status.NEW
+                        : (s.split(",")[3].equals("DONE") ? Status.DONE : Status.IN_PROGRESS));
                 String description = s.split(",")[4];
                 taskFromFile = new Task(name, description, id, status);
 
@@ -62,8 +94,8 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 int id = Integer.parseInt(s.split(",")[0]);
                 String type = s.split(",")[1];
                 String name = s.split(",")[2];
-                Status status = (s.split(",")[3].equals("New") ? Status.NEW
-                        : (s.split(",")[3].equals("Done") ? Status.DONE : Status.IN_PROGRESS));
+                Status status = (s.split(",")[3].equals("NEW") ? Status.NEW
+                        : (s.split(",")[3].equals("DONE") ? Status.DONE : Status.IN_PROGRESS));
                 String description = s.split(",")[4];
                 int myEpicId = Integer.parseInt(s.split(",")[5]);
                 taskFromFile = new Subtask(name, description, id, status, myEpicId);
@@ -72,33 +104,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         }
         return taskFromFile;
     }
-
-        //  =========  для сохранения и восстановления менеджера истории из CSV  =======
-
-    static String historyToString(HistoryManager manager) {
-        for (Task task : manager.getHistory()) {
-            
-        }
-        return null;
-    }
-
-    static List<Integer> historyFromString(String value) {
-        
-        
-        /*
-        Когда собираешь таски из файла, заведи временную мапу с тасками, ключ - id вэлью - таска.
-        Потом вытащи арайлист из последней строки файла и пройдись им по мапе
-        Добавляй вэлью(таску) в кастом линкедЛист (setHistory)
-         */
-
-
-        return null;
-    }
-
-    // === Напишите метод сохранения задачи в строку, или переопределите базовый.
-    String toString(Task task) {
-        return null;
-    }  // или переопределите базовый!?
 
     private void save() throws IOException {                        // сохранение изменений в файл
         List<Task> tmpStorage = new ArrayList<>();
@@ -112,7 +117,8 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
             tmpStorage.add(value);
         }
 
-        try (FileWriter fileWriter = new FileWriter("/path")) {  // !!!!!!!!!!!!!!! прописать путь!!!!!!!
+        try (FileWriter fileWriter = new FileWriter("storage.txt")) {
+
             fileWriter.write("id,type,name,status,description,epic" + "\n");
             int counter = 0;
             for (Task taskFromStorage : tmpStorage) {
@@ -132,40 +138,19 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 }
             }
             fileWriter.write("\n");
-            for (Task task : historyManager.getHistory()) {
-                fileWriter.write(task.getId());
-            }
-            // fileWriter.close();  не нужен, если try-with-resources
+            fileWriter.write(historyToString(historyManager));
 
         } catch (FileNotFoundException e) {
             System.out.println("Произошла ошибка во время записи файла.");
         }
     }
-/*
-Проверка работы нового менеджера
-Исключения вида IOException нужно отлавливать внутри метода save и кидать собственное непроверяемое исключение
+
+/* Исключения вида IOException нужно отлавливать внутри метода save и кидать собственное непроверяемое исключение
 ManagerSaveException. Благодаря этому можно не менять сигнатуру методов интерфейса менеджера.
-
-id,type,name,status,description,epic
-1,TASK,Task1,NEW,Description task1,
-2,EPIC,Epic2,DONE,Description epic2,
-3,SUBTASK,Sub Task2,DONE,Description sub task3,2
-
-2,3
-
-Сначала через запятую перечисляются все поля задач. Ниже находится список задач, каждая из них записана с новой 
-строки. Дальше — пустая строка, которая отделяет задачи от истории просмотров. 
-И заключительная строка — это идентификаторы задач из истории просмотров.
-Файл из нашего примера можно прочитать так: в трекер добавлены задача, эпик и подзадача. Эпик и подзадача 
-просмотрены
-и выполнены. Задача осталась в состоянии новой и не была просмотрена.
 
 try(IOException ex){ throw new ManagerSaveException("custom message", ex);}
 
-ManagerSaveException extends RuntimeException
-
-*/
-
+ManagerSaveException extends RuntimeException */
 
     @Override
     public void makeNewEpic(Epic epic) throws IOException {
@@ -298,6 +283,10 @@ ManagerSaveException extends RuntimeException
     public String toString() {
         return super.toString();
     }
+    // === Напишите метод сохранения задачи в строку, или переопределите базовый.
+    //   String toString(Task task) {
+    //        return null;
+    //    }  // или переопределите базовый!?
 }
 
 
