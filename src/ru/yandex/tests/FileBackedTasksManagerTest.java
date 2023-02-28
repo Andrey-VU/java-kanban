26c24c315d398ab38ec190f02d821ee40a91702b
Andrey-VU <avustjuzhanin@yandex.ru>
2023-02-28 19:02:39 +0300

    Refactor: исправить тесты с учётом передачи пути к файлам через конструктор

--- src/ru/yandex/tests/FileBackedTasksManagerTest.java
+++ src/ru/yandex/tests/FileBackedTasksManagerTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 class FileBackedTasksManagerTest extends TaskManagerTest {
     TaskManager inMemoryManager;
-    TaskManager recoveredFromFile;
+    TaskManager recoveredFromFile2;
     Task testTaskZ;
     Epic testEpicZ;
     Subtask testSubtaskZ;
@@ -29,12 +29,10 @@ class FileBackedTasksManagerTest extends TaskManagerTest {
 
     @BeforeEach
     public void beforeEach() throws IOException {
-        manager = Managers.getFileBackedManager();
-        inMemoryManager = Managers.getDefault();
-        recoveredFromFile = Managers.getFileBackedManager();
-        FileBackedTasksManager.fileIn = new File("storageTestIn.csv");
-        FileBackedTasksManager.fileOut = new File("storageTestOut1.csv");
-        recoveredFromFile = FileBackedTasksManager.loadFromFile(FileBackedTasksManager.fileIn);
+        manager = Managers.getFileBackedManager();              // инициализация для тестов абстрактного класса
+        inMemoryManager = Managers.getInMemoryTaskManager();
+        recoveredFromFile2 = FileBackedTasksManager.loadFromFile("storageTestIn.csv",
+                "storageTestOut1.csv");
 
         testTaskZ = new Task("Test_Task_name","Test_Task_description", 0, Status.NEW,
                 "01.01.2004--12:00", 3600);
@@ -57,83 +55,47 @@ class FileBackedTasksManagerTest extends TaskManagerTest {
     public void afterEach() throws IOException {
         manager.dellThemAll();
         inMemoryManager.dellThemAll();
-        recoveredFromFile.dellThemAll();
-        if (recoveredFromFile.getHistory() != null) {
-            recoveredFromFile.getHistory().clear();
+        recoveredFromFile2.dellThemAll();
+        if (recoveredFromFile2.getHistory() != null) {
+            recoveredFromFile2.getHistory().clear();
+            recoveredFromFile2.getPrioritizedTasks().clear();
         }
-        FileBackedTasksManager.fileIn = null;
-        FileBackedTasksManager.fileOut = null;
-        if (recoveredFromFile.getPrioritizedTasks() !=null) {
-            recoveredFromFile.getPrioritizedTasks().clear();
+        if (recoveredFromFile2.getPrioritizedTasks() !=null) {
+            recoveredFromFile2.getPrioritizedTasks().clear();
         }
-    }
-
-    @Test
-    public void shouldMakePrioritizedList() throws IOException {
-        String[] newTmpTask = {"4","TASK","Гитарный конкурс в Питере","NEW","Купить билеты",
-                "01.01.1999--12:00","3600"};
-        Task tmpTask = new Task(newTmpTask);
-        recoveredFromFile.makeNewTask(tmpTask);
-        List<Task> tmpPriority = new ArrayList<>();
-        tmpPriority.add(recoveredFromFile.getTaskById(4));
-        tmpPriority.add(recoveredFromFile.getSubTaskById(3));
-        tmpPriority.add(recoveredFromFile.getTaskById(1));
-
-        List<Task> priorityTest = recoveredFromFile.getPrioritizedTasks();
-        assertEquals(tmpPriority, priorityTest,
-                "Задачи не удалось ранжировать по времени старта");
-    }
-
-    @Test
-    public void shouldMakePrioritizedListWithoutData () throws IOException {
-
-        Task tmpTask4 = new Task("Гитарный конкурс в Питере","Купить билеты",4,Status.NEW);
-        Task tmpTask5 = new Task("Test_priority","Descr 5",5,Status.NEW);
-        recoveredFromFile.makeNewTask(tmpTask4);
-        recoveredFromFile.makeNewTask(tmpTask5);
-        List<Task> tmpPrioroty = new ArrayList<>();
-        tmpPrioroty.add(recoveredFromFile.getSubTaskById(3));
-        tmpPrioroty.add(recoveredFromFile.getTaskById(1));
-        tmpPrioroty.add(recoveredFromFile.getTaskById(4));
-        tmpPrioroty.add(recoveredFromFile.getTaskById(5));
-
-        List<Task> priorityTest = recoveredFromFile.getPrioritizedTasks();
-        assertEquals(tmpPrioroty, priorityTest,
-                "Ошибка, когда у задач не прописано время старта");
     }
 
         @Test
     public void shouldLoadFromFile() throws IOException {
-        Task taskFromFile = recoveredFromFile.getTaskById(1);
-        Epic epicFromFile = recoveredFromFile.getEpicById(2);
-        Subtask subtaskFromFile = recoveredFromFile.getSubTaskById(3);
-        List newHistory = recoveredFromFile.getHistory();
-
-        assertEquals(inMemoryManager.getTaskById(1).toString(), taskFromFile.toString(),
-                "Task из файла загружен не корректно");
-        assertEquals(inMemoryManager.getEpicById(2), epicFromFile,
-                "Epic из файла загружен не корректно");
-        assertEquals(inMemoryManager.getSubTaskById(3), subtaskFromFile,
-                "Subtask из файла загружен не корректно");
-        assertEquals(historyForTest, newHistory,
-                "История из файла загружена не корректно");
-    }
+        Task taskFromFile = recoveredFromFile2.getTaskById(1);
+        Epic epicFromFile = recoveredFromFile2.getEpicById(2);
+        Subtask subtaskFromFile = recoveredFromFile2.getSubTaskById(3);
+        List newHistory = new ArrayList<>();
+        if (recoveredFromFile2.getHistory() != null) {
+            newHistory = recoveredFromFile2.getHistory();
+        }
+            assertEquals(inMemoryManager.getTaskById(1), taskFromFile,
+                    "Task из файла загружен не корректно");
+            assertEquals(inMemoryManager.getEpicById(2), epicFromFile,
+                    "Epic из файла загружен не корректно");
+            assertEquals(inMemoryManager.getSubTaskById(3), subtaskFromFile,
+                    "Subtask из файла загружен не корректно");
+            assertEquals(historyForTest, newHistory,
+                    "История из файла загружена не корректно");
+        }
 
     @Test
     public void shouldSaveToFile() throws IOException {
-        FileBackedTasksManager.fileIn = new File("storageTestOut1.csv");
-        FileBackedTasksManager.fileOut = new File("storageTestOut2.csv");
+        TaskManager recoveredFromFile3
+                = FileBackedTasksManager.loadFromFile("storageTestOut1.csv", "storageTestOut2.csv");
 
-        recoveredFromFile.dellThemAll();
-        recoveredFromFile.getHistory().clear();
+        Task taskFromFile2 = recoveredFromFile3.getTaskById(1);
+        Epic epicFromFile2 = recoveredFromFile3.getEpicById(2);
+        Subtask subtaskFromFile2 = recoveredFromFile3.getSubTaskById(3);
 
-        recoveredFromFile = FileBackedTasksManager.loadFromFile(FileBackedTasksManager.fileIn);
+        //Task testTask =
 
-        Task taskFromFile2 = recoveredFromFile.getTaskById(1);
-        Epic epicFromFile2 = recoveredFromFile.getEpicById(2);
-        Subtask subtaskFromFile2 = recoveredFromFile.getSubTaskById(3);
-
-        assertEquals(inMemoryManager.getTaskById(1).toString(), taskFromFile2.toString(),
+        assertEquals(inMemoryManager.getTaskById(1), taskFromFile2,
                 "Сохранение задач в файл некорректно");
         assertEquals(inMemoryManager.getEpicById(2), epicFromFile2,
                 "Epic из файла загружен не корректно");
@@ -143,34 +105,36 @@ class FileBackedTasksManagerTest extends TaskManagerTest {
 
     @Test
     public void loadWhenEpicWithoutSubtask() throws IOException {
-        FileBackedTasksManager.fileIn = new File("storage_In_TestEpicWithoutSubtask.csv");
-        FileBackedTasksManager.fileOut = new File("storage_Out_TestEpicWithoutSubtask.csv");
+        // recoveredFromFile2 = FileBackedTasksManager.loadFromFile("storageTestIn.csv",
+        //                "storageTestOut1.csv");
 
-        TaskManager newManager = Managers.getDefault();
-        // TaskManager newManager2 = Managers.getDefault();
+        TaskManager recoveredFromFileForEpicWithoutSub
+                = FileBackedTasksManager.loadFromFile("storage_In_TestEpicWithoutSubtask.csv",
+                "storage_Out_TestEpicWithoutSubtask.csv");
+
+        TaskManager newManager = Managers.getInMemoryTaskManager();
+
 
         Epic epicWithoutSb = new Epic("Without sub name", "Without sub discr",
                 0, Status.NEW);
         newManager.makeNewEpic(epicWithoutSb);
         Epic expectedEpic = newManager.getEpicById(epicWithoutSb.getId());
 
-        recoveredFromFile = FileBackedTasksManager.loadFromFile(FileBackedTasksManager.fileIn);
-        assertEquals(expectedEpic, recoveredFromFile.getEpicById(1),
+        recoveredFromFile2 = FileBackedTasksManager.loadFromFile("storage_In_TestEpicWithoutSubtask.csv",
+                "storage_Out_TestEpicWithoutSubtask.csv");
+        assertEquals(expectedEpic, recoveredFromFile2.getEpicById(1),
                 "Эпик без подзадач не создан или создан с ошибками");
-        assertNull(recoveredFromFile.getListSubtasksOfEpic(inMemoryManager.getEpicById(1)),
+        assertNull(recoveredFromFile2.getListSubtasksOfEpic(inMemoryManager.getEpicById(1)),
                 "создание Эпика без подзадач прошло с ошибками");
     }
 
     @Test
     public void loadWhenFileWithoutHistory() throws IOException {
-        FileBackedTasksManager newRecoveredFromFile = new FileBackedTasksManager();
+        FileBackedTasksManager newRecoveredFromFile =
+                FileBackedTasksManager.loadFromFile("storage_In_WithoutHistory.csv",
+                        "storage_Out_WithoutHistory.csv");
 
         inMemoryManager.getHistory().clear();
-
-        FileBackedTasksManager.fileIn = new File("storage_In_WithoutHistory.csv");
-        FileBackedTasksManager.fileOut = new File("storage_Out_WithoutHistory.csv");
-
-        newRecoveredFromFile = FileBackedTasksManager.loadFromFile(FileBackedTasksManager.fileIn);
 
         Task taskFromMemory = inMemoryManager.getTaskById(1);
         Epic epicFromMemory = inMemoryManager.getEpicById(2);
@@ -184,6 +148,39 @@ class FileBackedTasksManagerTest extends TaskManagerTest {
               "Task без истории не создан или создан с ошибками");
         assertEquals(epicFromMemory, epicFromFile, "Epic без истории не создан или создан с ошибками");
         assertEquals(subtaskFromMemory, subtaskFromFile, "SubTask без истории не создан или создан с ошибками");
-
     }
+    @Test
+    public void shouldMakePrioritizedList() throws IOException {
+        String[] newTmpTask = {"4","TASK","Гитарный конкурс в Питере","NEW","Купить билеты",
+                "01.01.1999--12:00","3600"};
+        Task tmpTask = new Task(newTmpTask);
+        recoveredFromFile2.makeNewTask(tmpTask);
+        List<Task> tmpPriority = new ArrayList<>();
+        tmpPriority.add(recoveredFromFile2.getTaskById(4));
+        tmpPriority.add(recoveredFromFile2.getSubTaskById(3));
+        tmpPriority.add(recoveredFromFile2.getTaskById(1));
+
+        List<Task> priorityTest = recoveredFromFile2.getPrioritizedTasks();
+        assertEquals(tmpPriority, priorityTest,
+                "Задачи не удалось ранжировать по времени старта");
+    }
+
+    @Test
+    public void shouldMakePrioritizedListWithoutData () throws IOException {
+
+        Task tmpTask4 = new Task("Гитарный конкурс в Питере","Купить билеты",4,Status.NEW);
+        Task tmpTask5 = new Task("Test_priority","Descr 5",5,Status.NEW);
+        recoveredFromFile2.makeNewTask(tmpTask4);
+        recoveredFromFile2.makeNewTask(tmpTask5);
+        List<Task> tmpPrioroty = new ArrayList<>();
+        tmpPrioroty.add(recoveredFromFile2.getSubTaskById(3));
+        tmpPrioroty.add(recoveredFromFile2.getTaskById(1));
+        tmpPrioroty.add(recoveredFromFile2.getTaskById(4));
+        tmpPrioroty.add(recoveredFromFile2.getTaskById(5));
+
+        List<Task> priorityTest = recoveredFromFile2.getPrioritizedTasks();
+        assertEquals(tmpPrioroty, priorityTest,
+                "Ошибка, когда у задач не прописано время старта");
+    }
+
 }
