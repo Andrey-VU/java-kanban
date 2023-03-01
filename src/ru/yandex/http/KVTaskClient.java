adbad382bb02c870e8bb143306a009f7fba1e7b5
Andrey-VU <avustjuzhanin@yandex.ru>
2023-03-01 21:33:26 +0300

    New methods: добавить поля и архитектуру методов

--- src/ru/yandex/http/KVTaskClient.java
+++ src/ru/yandex/http/KVTaskClient.java
@@ -1,77 +1,58 @@
 package ru.yandex.http;
+import com.sun.net.httpserver.HttpExchange;
+import ru.yandex.tmanager.HttpTaskManager;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-/*
-1. HttpTaskManager отправляет запрос на получение/сохранение/удаление/обновление задачи HttpTaskServer'у;
-2. HttpTaskServer обрабатывает запрос:
-   - вызывает экземпляр FileManager, чтобы правильным образом обработать запрос HttpTaskManager;
-   - передаёт поручение KVTaskClient'у, (который нужен для того, чтобы взаимодействовать с KVServerom);
-   - отчитывается перед менеджером о том, что запрос получен/отправлен или произошла неувязка;
-3. KVTaskClient обработывает полученное поручение и обращается в хранилище KVServer, чтобы достать/изменить/удалить
-    - отчитывается перед KVTaskServer о ходе выполнения;
-4. KVServer - меняет состояние хранилища (файла) на сервери в соответствии с запросом; отчитывается об этом;
-
-
-У KVTaskClient 3 метода для методов register/save/load из KVServer. Каждый метод создает http запрос, отправляет его,
-и сообщает статус.
-В случаи GET-запросов получает ответ от KVсервера и передаёт его менеджеру. Менеджер парсит JSON в нужный тип.
-При POST-запросах используем при создании метод POST(HttpRequest.BodyPublishers.ofString(“Указываем json, который
- хотим передать на сервер”)).
-Ключ передаем через параметр пути запроса.
-
-В KVTaskClient в URI нужно подставить значение токена (поле apiToken из KVServer). Как его лучше достать?
-Создать дублирующее поле apiToken и инициализировать его, возвращая значение из метода register?
-Сохранять токен в клиенте
-//    При создании KVTaskClient учтите следующее: Конструктор принимает URL к серверу хранилища и
-//    регистрируется. При регистрации выдаётся токен (API_TOKEN), который нужен при работе с сервером.
-
- */
-
 public class KVTaskClient {
+    private KVServer KVServer;
+    private HttpTaskManager httpTaskManager;
     private final String apiToken;
     private HttpClient client = HttpClient.newHttpClient();
+
 
     //В KVTaskClient в URI нужно подставить значение токена (поле apiToken из KVServer)
     public KVTaskClient(String apiToken) throws IOException, InterruptedException {
         this.apiToken = apiToken;      }
 
-    public HttpResponse<String> send(HttpRequest requestAllTasks, HttpResponse.BodyHandler<String> ofString) {
+    void put(String key, String json) {
+     //   Метод должен сохранять состояние менеджера задач через запрос POST /save/<ключ>?API_TOKEN=.
+    }
+    String load(String key) {
+//  Метод должен возвращать состояние менеджера задач через запрос GET /load/<ключ>?API_TOKEN=.
+////    Далее проверьте код клиента в main.
         return null;
     }
 
-    // 1. Получить все задачи Task
+    void register(HttpExchange h) {
 
-
-//    Метод void put(String key, String json) должен сохранять состояние менеджера задач через
-//    запрос POST /save/<ключ>?API_TOKEN=.
-//    Метод String load(String key) должен возвращать состояние менеджера задач через запрос
-//    GET /load/<ключ>?API_TOKEN=.
-//    Далее проверьте код клиента в main.
-//    Для этого запустите KVServer, создайте экземпляр KVTaskClient. Затем сохраните
+    }
+//
+//    //    Для этого запустите KVServer, создайте экземпляр KVTaskClient. Затем сохраните
 //    значение под разными ключами и проверьте, что при запросе возвращаются нужные данные.
-//            Удостоверьтесь, что если
-//    изменить значение, то при повторном вызове вернётся уже не старое, а новое.
+//            Удостоверьтесь, что если изменить значение, то при повторном вызове вернётся уже не старое, а новое.
+//    public HttpResponse<String> send(HttpRequest requestAllTasks, HttpResponse.BodyHandler<String> ofString) {
+//        return null;
+//    }
 
-//  KVTaskClient - создает http запросы по примеру из задания
-//  к KVServer - 3 типа запросов - register/save/load
+    /*
+В KVTaskClient в URI нужно подставить значение токена (поле apiToken из KVServer). Как его лучше достать?
+Создать дублирующее поле apiToken и инициализировать его, возвращая значение из метода register?
+Сохранять токен в клиенте  При создании KVTaskClient учтите следующее: Конструктор принимает URL к серверу хранилища и
+регистрируется. При регистрации выдаётся токен (API_TOKEN), который нужен при работе с сервером.
+ */
+
+//  KVTaskClient - создает http запросы по примеру из задания к KVServer - 3 типа запросов - register/save/load
     // запросы register
-
     // запросы save
-
-
     // запросы load
-
-
 // к HttpTaskServer запросы типа GET, POST, DELETE
 
-
-
-
-    // получаем экземпляр класса-строителя
+// получаем экземпляр класса-строителя
 //    HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
 //
 //    // создаём объект, описывающий HTTP-запрос
